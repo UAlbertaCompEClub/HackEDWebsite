f1196a0d6ed89d51515cec3f21a1cc7cd53462ad
ChrisChrisLoLo <sporewoh@gmail.com>
2019-09-12 09:10:11 -0600

    Add constants to later integration to ease updating information
    Add moment.js for easier date handling

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+import moment from "moment";
+
+export const EVENT_NAME = "HackEd";
+export const EVENT_NAME_YEAR = EVENT_NAME+" 2020";
+export const EVENT_START_TIME = moment("2555-02-08 09:30");
+export const EVENT_END_TIME = moment("2555-02-08 09:30");
+export const EVENT_LOCATION = "Not DICE";
