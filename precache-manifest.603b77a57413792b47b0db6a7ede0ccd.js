3d0a9b43faee80c9c10faa561bb40fd325f3086b
ChrisChrisLoLo <sporewoh@gmail.com>
2019-09-29 21:05:48 -0600

    Updates

---
+++ precache-manifest.603b77a57413792b47b0db6a7ede0ccd.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95265eb0cb31840a160166c326bda328",
+    "url": "/HackEDWebsiteBetaPage/index.html"
+  },
+  {
+    "revision": "9df13fb0d211289af566",
+    "url": "/HackEDWebsiteBetaPage/static/css/2.0ec30c4e.chunk.css"
+  },
+  {
+    "revision": "ecd6b323c6027cc2c847",
+    "url": "/HackEDWebsiteBetaPage/static/css/main.a850100b.chunk.css"
+  },
+  {
+    "revision": "9df13fb0d211289af566",
+    "url": "/HackEDWebsiteBetaPage/static/js/2.1b8d238c.chunk.js"
+  },
+  {
+    "revision": "ecd6b323c6027cc2c847",
+    "url": "/HackEDWebsiteBetaPage/static/js/main.0ff1888c.chunk.js"
+  },
+  {
+    "revision": "36fd3fb42d7ed1a69861",
+    "url": "/HackEDWebsiteBetaPage/static/js/runtime~main.567b11f1.js"
+  },
+  {
+    "revision": "e2f07f785a4cc283f18bcb489efb7c92",
+    "url": "/HackEDWebsiteBetaPage/static/media/Amii.e2f07f78.png"
+  },
+  {
+    "revision": "9eec68a925702af341f2a4d92915ff26",
+    "url": "/HackEDWebsiteBetaPage/static/media/BrandMakers.9eec68a9.png"
+  },
+  {
+    "revision": "4e0c304f894934d7c6dd7e64bb757e78",
+    "url": "/HackEDWebsiteBetaPage/static/media/CityOfEdmonton.4e0c304f.png"
+  },
+  {
+    "revision": "63739d0759522d9b27f11be1c4d15d7d",
+    "url": "/HackEDWebsiteBetaPage/static/media/DevFacto.63739d07.png"
+  },
+  {
+    "revision": "15c8062d4e209429f1eedb8fe9528206",
+    "url": "/HackEDWebsiteBetaPage/static/media/FacultyOfEngineering.15c8062d.jpg"
+  },
+  {
+    "revision": "01a8c8791cbb799819dc50177b49a173",
+    "url": "/HackEDWebsiteBetaPage/static/media/HackED.01a8c879.png"
+  },
+  {
+    "revision": "359e7a424dc5d7534848d5a414bfffab",
+    "url": "/HackEDWebsiteBetaPage/static/media/Jobber.359e7a42.jpg"
+  },
+  {
+    "revision": "0db5e6cc3f0fec7c59f4ee6b6a6d2933",
+    "url": "/HackEDWebsiteBetaPage/static/media/Servus.0db5e6cc.jpg"
+  },
+  {
+    "revision": "a196f61500a4864ca7b7ee7ccbec6d35",
+    "url": "/HackEDWebsiteBetaPage/static/media/StartupEdmonton.a196f615.png"
+  },
+  {
+    "revision": "037f47668d244493bd297d898484a19f",
+    "url": "/HackEDWebsiteBetaPage/static/media/StickerMule.037f4766.png"
+  },
+  {
+    "revision": "0d506e0ed96895ee3e5f23dd557dae85",
+    "url": "/HackEDWebsiteBetaPage/static/media/Telus.0d506e0e.JPG"
+  },
+  {
+    "revision": "1bf0d3371651062f401c2a335b8cbd10",
+    "url": "/HackEDWebsiteBetaPage/static/media/ThePod.1bf0d337.png"
+  },
+  {
+    "revision": "a2ab4acbf16bb12327f308326ba90282",
+    "url": "/HackEDWebsiteBetaPage/static/media/WillowglenSystems.a2ab4acb.jpg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/HackEDWebsiteBetaPage/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/HackEDWebsiteBetaPage/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/HackEDWebsiteBetaPage/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/HackEDWebsiteBetaPage/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/HackEDWebsiteBetaPage/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/HackEDWebsiteBetaPage/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/HackEDWebsiteBetaPage/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/HackEDWebsiteBetaPage/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/HackEDWebsiteBetaPage/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/HackEDWebsiteBetaPage/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/HackEDWebsiteBetaPage/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/HackEDWebsiteBetaPage/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/HackEDWebsiteBetaPage/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/HackEDWebsiteBetaPage/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/HackEDWebsiteBetaPage/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/HackEDWebsiteBetaPage/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.836b50ce8ffe7c5e4864a4cd46d9b945.js"
+  "/HackEDWebsiteBetaPage/precache-manifest.603b77a57413792b47b0db6a7ede0ccd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/HackEDWebsiteBetaPage/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
