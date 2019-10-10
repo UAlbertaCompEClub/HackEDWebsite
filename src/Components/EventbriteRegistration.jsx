f56cc7a213bd06e64d6fddc9be522426145fab22
ChrisChrisLoLo <sporewoh@gmail.com>
2019-10-10 09:07:57 -0600

    Add registration disclaimer

--- src/Components/EventbriteRegistration.jsx
+++ src/Components/EventbriteRegistration.jsx
@@ -13,6 +13,17 @@ class EventbriteRegistration extends Component {
         </Row>
         <Row>
           <Col>
+            <p className="text-center">
+              Remember that you must be in highschool or in your first or second year of university, and have been in no more than one prior hackathon in order to participate.
+            </p>
+            <p className="text-center">
+              Rare exceptions can be made if participants have little to no prior experience with hacking or programming in general.
+              If you believe you are a novice hacker, contact hacked@compeclub.com for more information.
+            </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
             <div id="eventbrite-widget-container-75763084425" className="mt-5"/>
           </Col>
         </Row>
