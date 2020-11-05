cdb9b1252214bbb6c14a1786334de9c6dc93a927
Samuel Lojpur <samlojpur@gmail.com>
2020-11-05 14:46:09 -0700

    Add shoutouts to adasteam and uacs

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,62 @@
 import React, {Component} from 'react';
+import { Grid, Image, List } from 'semantic-ui-react'
 
-import { Grid, List } from 'semantic-ui-react'
+import {EVENT_NAME} from "../constants";
+
+import CompEClubLogo from "../logos/CompEClub.jpg";
+import AdasTeamLogo from "../logos/AdasTeam.png";
+
 
 class Footer extends Component {
     render() {
         return (
+            <>
+            <Grid
+            id="Organizers"
+            centered
+            alignItems="center"
+            style={{
+            background: "#FFFFFF",
+            marginTop: 0,
+            marginBottom: 40
+            }}
+            columns={3}
+            container
+            stackable
+            relaxed="very"
+            >         
+            <Grid.Row centered>
+                <Grid.Column width={2}>
+                    <Image
+                    src={AdasTeamLogo}
+                    fluid
+                    as="a"
+                    href="https://www.adasteam.ca/"
+                    target="_blank"
+                    />
+                </Grid.Column>
+                <Grid.Column  width={2}>
+                    <Image
+                    src={CompEClubLogo}
+                    fluid
+                    as="a"
+                    href="https://www.compeclub.com/"
+                    target="_blank"
+                    />
+                </Grid.Column>
+                <Grid.Column width={2}>
+                    <a href="http://uacs.ca/" class="nav-link">
+                        <h1
+                        style={{
+                            paddingTop: 11,
+                            color: '#3EA45F',
+                            fontWeight: 600,
+                        }}
+                        >UACS</h1>
+                    </a>
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>
             <Grid 
             centered
             className = "bg-white"
@@ -12,6 +64,15 @@ class Footer extends Component {
             container
             stackable
             relaxed='very'>
+                <Grid.Row centered>
+                <Grid.Column width={12}>
+
+                    <h4>
+                    The Computer Engineering Club would like to give a special thank you to Ada's Team and UACS, without whom {EVENT_NAME} would not be possible.
+                    </h4>
+                    </Grid.Column>
+                </Grid.Row>
+
                 <Grid.Row centered>
                     <Grid.Column>
                         <h4>
@@ -54,7 +115,7 @@ class Footer extends Component {
                         <p style={{textAlign: "center"}}>© University of Alberta Computer Engineering Club</p>
                     </Grid.Column>
                 </Grid.Row>
-            </Grid>
+            </Grid></>
         );
     }
 }
