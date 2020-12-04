faadc31a91465948df8732c420d3e7f0e71660f3
Samuel Lojpur <samlojpur@gmail.com>
2020-12-04 14:00:49 -0700

    fix dates

--- src/constants.js
+++ src/constants.js
@@ -2,8 +2,8 @@ import moment from "moment";
 
 export const EVENT_NAME = "HackEd";
 export const EVENT_NAME_FULL = EVENT_NAME + " 2021";
-export const EVENT_START_TIME = moment("2021-01-18 09:30");
-export const EVENT_END_TIME = moment("2021-01-19 05:30");
+export const EVENT_START_TIME = moment("2021-01-16 09:30");
+export const EVENT_END_TIME = moment("2021-01-17 05:30");
 export const EVENT_LOCATION = "Donadeo Innovation Centre for Engineering";
 export const EVENT_LOCATION_ADDRESS = "116 St NW, Edmonton, AB T6G 2E1";
 export const PARTICIPANT_COUNT = "400";
