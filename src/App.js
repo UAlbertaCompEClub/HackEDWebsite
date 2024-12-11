007c10d1e7d723476c0aeb8544c49d7d7070aa53
Ruchali Aery <112783914+ruchali1@users.noreply.github.com>
2024-12-11 10:21:26 -0700

    new feb dates

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ class App extends Component {
                 One of Alberta's Largest Student Run Hackathons
               </h2>
               <h1 style={{ color: "#FFFFFF",fontFamily: 'Open Sans'}}>
-                January 3rd-5th, 2025
+                February 15th - 17th, 2025
               </h1> 
               <hr style={{color: "#FFFFFF", width: "40%", fontFamily: 'Open Sans'}}></hr>
               <h5 style={{ color: "#FFFFFF",fontFamily: 'Open Sans'}} >{EVENT_LOCATION}
